4871c35309cd992499f88bbde387538744cbc523
Rodolfo <rodolfo.pinto@tabmedia.com.br>
2015-06-08 15:19:38 -0300

    Aula 3 js DOM

--- projeto/calculaimc.js
+++ projeto/calculaimc.js
@@ -1,14 +1,46 @@
 // peso / altura * altura
 
-var peso = 100;
-var altura= 2;
-var nome = "Rodolfo";
+var tdPeso = document.getElementById("peso-1");
+var tdAltura = document.getElementById("altura-1");
 
+
+var peso = tdPeso.textContent;
+var altura= tdAltura.textContent;
+
+var paciente = {"peso":peso, "altura":altura };
 
 if(altura != 0){
-var imc = peso / (altura*altura);
+var imc = paciente.peso / (paciente.altura*paciente.altura);
+	
+	var tdimc=document.getElementById("imc-1");
+	tdimc.textContent = imc;
+
 console.log(imc);
-console.log("Seu nome e "+nome+ " seu imc e "+imc);
+
+}
+else{
+
+	console.log("Altura é igual a 0");
+
+}
+
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
+
+
+var peso = tdPeso.textContent;
+var altura= tdAltura.textContent;
+
+var paciente = {"peso":peso, "altura":altura };
+
+if(altura != 0){
+var imc = paciente.peso / (paciente.altura*paciente.altura);
+	
+	var tdimc=document.getElementById("imc-2");
+	tdimc.textContent = imc;
+
+console.log(imc);
+
 }
 else{
 
