e89a63e1b5cb9ee0fc2fd53cea2cab7f8cc05e6f
Rodolfo <rodolfo.into@tabmedia.com.br>
2015-06-08 12:34:41 -0300

    Aula 2 js variaveis e ifs

---
+++ projeto/calculaimc.js
@@ -0,0 +1,17 @@
+// peso / altura * altura
+
+var peso = 100;
+var altura= 2;
+var nome = "Rodolfo";
+
+
+if(altura != 0){
+var imc = peso / (altura*altura);
+console.log(imc);
+console.log("Seu nome e "+nome+ " seu imc e "+imc);
+}
+else{
+
+	console.log("Altura é igual a 0");
+
+}
