4daa3cafdb4133107a6f347e9e6658d651a1f351
Rodolfo <rodolfo.pinto@tabmedia.com.br>
2015-06-09 11:54:24 -0300

    Aula 4 JS Loop

--- projeto/calculaimc.js
+++ projeto/calculaimc.js
@@ -1,19 +1,53 @@
 // peso / altura * altura
 
+/*var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2");
+
+
+var peso2 = tdPeso.textContent;
+var altura2= tdAltura.textContent;
+
+var paciente2 = {"peso":peso2, "altura":altura2 };
+
 var tdPeso = document.getElementById("peso-1");
 var tdAltura = document.getElementById("altura-1");
 
 
-var peso = tdPeso.textContent;
-var altura= tdAltura.textContent;
+var peso1 = tdPeso.textContent;
+var altura1= tdAltura.textContent;
 
-var paciente = {"peso":peso, "altura":altura };
+var paciente1= {"peso":peso1, "altura":altura1 };
 
-if(altura != 0){
-var imc = paciente.peso / (paciente.altura*paciente.altura);
+var pacientes = [paciente1 ,paciente2];*/
+
+
+var trsPacientes = document.getElementsByClassName("paciente");
+
+
+
+var posicaoAtual = 0;
+
+while (posicaoAtual <= trsPacientes.length - 1){
+
+	var pacienteTr = trsPacientes[posicaoAtual];
+
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+    
+    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+
+
+    var paciente = {nome: tdNome.textContent, peso: tdPeso.textContent , altura: tdAltura.textContent};
+
+if(paciente.altura != 0){
+
+    var imc = paciente.peso / (paciente.altura*paciente.altura);
 	
-	var tdimc=document.getElementById("imc-1");
-	tdimc.textContent = imc;
+	trsPacientes
+    tdImc.textContent = imc;
+	
+   tdImc.textContent = imc;
 
 console.log(imc);
 
@@ -24,26 +58,7 @@ else{
 
 }
 
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
-
-
-var peso = tdPeso.textContent;
-var altura= tdAltura.textContent;
-
-var paciente = {"peso":peso, "altura":altura };
-
-if(altura != 0){
-var imc = paciente.peso / (paciente.altura*paciente.altura);
-	
-	var tdimc=document.getElementById("imc-2");
-	tdimc.textContent = imc;
-
-console.log(imc);
-
+posicaoAtual++;
 }
-else{
 
-	console.log("Altura é igual a 0");
 
-}
