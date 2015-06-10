29109cf048611427c61824aba25c15c0c29b4c07
Rodolfo <rodolfo.pinto@tabmmedia.com.br>
2015-06-10 12:51:04 -0300

    Aula 6 JS eventos

--- projeto/calculaimc.js
+++ projeto/calculaimc.js
@@ -1,56 +1,54 @@
+function calculaTodosImcs(){
+
+        var trsPacientes = document.getElementsByClassName("paciente");
+
+        percorreArray(trsPacientes,
+
+        function (pacienteTr){
+
+            var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
+            var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+            var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+            
+           
+            var pacienteAtual = {nome: tdNome.textContent, 
+                                 peso: tdPeso.textContent ,         
+                                 altura: tdAltura.textContent,
+                                 pegaImc : function(){
+
+                                     if(this.altura != 0){
+
+                                            var imc = this.peso / (this.altura*this.altura);
+                                            return imc; 
+                                           
+                                        }
+                                     else{
+
+                                            console.log("Altura é igual a 0");
+
+                                        }
+
+                                 }};
+
+            var imc = pacienteAtual.pegaImc();
+            
+            var tdImc= pacienteTr.getElementsByClassName("info-imc")[0];
+           
+            tdImc.textContent = imc;
+            
+            console.log(imc);
+
+            
+        });
+
+}
+
+var botao = document.getElementById("calcula-imcs");
+botao.onclick = calculaTodosImcs;
 
 
-var trsPacientes = document.getElementsByClassName("paciente");
+botao.addEventListener("click",calculaTodosImcs);
+botao.addEventListener("click",function() {
 
-
-
-percorreArray(trsPacientes,
-
-function (pacienteTr){
-
-
-
-    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0];
-    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
-    
-   
-
-
-    var pacienteAtual = {nome: tdNome.textContent, 
-                         peso: tdPeso.textContent ,         
-                         altura: tdAltura.textContent,
-                         pegaImc : function(){
-
-                             if(this.altura != 0){
-
-                                    var imc = this.peso / (this.altura*this.altura);
-                                    return imc; 
-                                   
-                                }
-                             else{
-
-                                    console.log("Altura é igual a 0");
-
-                                }
-
-
-
-                         }};
-
-    var imc = pacienteAtual.pegaImc();
-    
-    var tdImc= pacienteTr.getElementsByClassName("info-imc")[0];
-   
-    tdImc.textContent = imc;
-    
-    
-
-    console.log(imc);
-
-	
-
-
+    console.log("Calcula");
 });
-
-
